b32077649e4190c8dd50c1b21d2c55847b33fa30
Telesphoreo <craftyswans@gmail.com>
2018-07-31 11:03:27 -0700

    what

--- src/main/java/me/totalfreedom/totalfreedommod/util/DepreciationAggregator.java
+++ src/main/java/me/totalfreedom/totalfreedommod/util/DepreciationAggregator.java
@@ -16,6 +16,11 @@ public class DepreciationAggregator
         return entity.getTargetBlock(transparent, maxDistance);
     }
 
+    public static byte getData_Block(Block block)
+    {
+        return block.getData();
+    }
+
     public static OfflinePlayer getOfflinePlayer(Server server, String name)
     {
         return server.getOfflinePlayer(name);
