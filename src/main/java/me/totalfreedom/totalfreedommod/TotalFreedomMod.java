734f2c7d620a68bac20b1920ff7da0d336c12bc6
Telesphoreo <craftyswans@gmail.com>
2018-08-04 03:02:57 -0700

    Update WorldEdit for 1.13

--- src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
+++ src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
@@ -245,7 +245,6 @@ public class TotalFreedomMod extends AeroPlugin<TotalFreedomMod>
         btb = bridges.registerService(BukkitTelnetBridge.class);
         cpb = bridges.registerService(CoreProtectBridge.class);
         esb = bridges.registerService(EssentialsBridge.class);
-        // Disabled so I can at least load TFM as these plugins aren't updated
         ldb = bridges.registerService(LibsDisguisesBridge.class);
         web = bridges.registerService(WorldEditBridge.class);
         wgb = bridges.registerService(WorldGuardBridge.class);
